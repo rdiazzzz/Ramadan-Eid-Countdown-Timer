464d7bf81948a04008a14e020f2fc82476249104
zenkaa7 <zenkaayo@gmail.com>
2022-05-17 17:39:04 +0700

    update :D

--- app.js
+++ app.js
@@ -3,22 +3,47 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
-const newRamadhanDate = new Date('April 2 2022 00:00:00');
+let interval;
 
-//countdown
-function updateCountdown() {
-    const currentTime = new Date();
-    const diff = newRamadhanDate - currentTime;
+const startTimer = () => {
+    const newRamadhanDate = new Date('March 22, 2023 00:00:00').getTime();
 
-    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const m = Math.floor(diff / 1000 / 60) % 60;
-    const s = Math.floor(diff / 1000) % 60;
+    interval = setInterval(() => {
 
-    days.innerHTML = d;
-    hours.innerHTML = h < 10 ? '0' + h : h;
-    minutes.innerHTML = m < 10 ? '0' + m : m;
-    seconds.innerHTML = s < 10 ? '0' + s : s;
+        const currentTime = new Date();
+        const distance = newRamadhanDate - currentTime;
+
+        const d = Math.floor(distance / 1000 / 60 / 60 / 24);
+        const h = Math.floor(distance / 1000 / 60 / 60) % 24;
+        const m = Math.floor(distance / 1000 / 60) % 60;
+        const s = Math.floor(distance / 1000) % 60;
+
+        if (distance < 0) {
+            clearInterval(interval)
+
+            days.innerHTML = "Happy"
+            hours.innerHTML = "Ra"
+            minutes.innerHTML = "ma"
+            seconds.innerHTML = "dan"
+        } else {
+            days.innerHTML = d;
+            hours.innerHTML = h < 10 ? '0' + h : h;
+            minutes.innerHTML = m < 10 ? '0' + m : m;
+            seconds.innerHTML = s < 10 ? '0' + s : s;
+        }
+    });
 }
 
-setInterval(updateCountdown, 1000);
+setInterval(startTimer, 1000);
+
+//countdown
+// function updateCountdown() {
+
+
+
+
+
+
+// }
+
+// 
